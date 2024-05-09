1510813d0b259aeb8e079098e454d295a30d2937
FranPi14 <fra.pisty@gmail.com>
2024-05-09 18:27:31 +0200

    add view

--- UI/view.py
+++ UI/view.py
@@ -8,38 +8,51 @@ class View(ft.UserControl):
         self._page = page
         self._page.title = "Template application using MVC and DAO"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
-        self.txt_result = None
+        self._ddyear = None
+        self._ddcolor = None
+        self.btn_graph = None
+        self.txtOut = None
         self.txt_container = None
+
+        self._ddnode = None
+        self.btn_search = None
+        self.txtOut2 = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("TdP 2024 - Lab11: Prova tema d'esame", color="blue", size=24)
         self._page.controls.append(self._title)
 
         #ROW with some controls
         # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
+        self._ddyear = ft.Dropdown(label="Anno")
+        self._ddcolor = ft.Dropdown(label="Colore")
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
+        # button for the "creat graph" reply
+        self.btn_graph = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handle_graph)
+        row1 = ft.Row([self._ddyear,self._ddcolor, self.btn_graph],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
 
+        self._controller.fillDD()
+
         # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
-        self._page.controls.append(self.txt_result)
+        self.txtOut = ft.ListView(expand=1, spacing=10, padding=10, auto_scroll=True)
+        self._page.controls.append(self.txtOut)
+
+        self._ddnode = ft.Dropdown(label="Product")
+        self.btn_search = ft.ElevatedButton(text="Cerca Percorso", on_click=self._controller.handle_search)
+        row2 = ft.Row([self._ddnode, self.btn_search],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row2)
+
+        self.txtOut2 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txtOut2)
         self._page.update()
 
     @property
